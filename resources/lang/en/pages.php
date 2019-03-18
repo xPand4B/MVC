de5d47b476fccb41407f0f77b3ebf435e7a4f4c7
xPand4B <xpand.4beatz@gmail.com>
2019-03-18 10:11:32 +0100

    Added translation files

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Eric Heinzl <eric.heinzl@gmail.com>
+ * @package xPand4B/Portfolio
+ * 
+ * @copyright 2019 Eric Heinzl
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain all page specific content such as
+    | the title, headline/breadcrumb, etc.
+    |
+    */
+    'home' => [
+        'welcome' => 'Welcome'
+    ]
+];
