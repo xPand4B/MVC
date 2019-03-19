9b3dc669e3874e161b60a982c44b55db8fc7f99b
xPand4B <xpand.4beatz@gmail.com>
2019-03-19 21:36:25 +0100

    Added app config file

---
+++ config/app.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Eric Heinzl <eric.heinzl@gmail.com>
+ * @package xPand4B/Portfolio
+ * 
+ * @copyright 2019 Eric Heinzl
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Supported application locales
+    |--------------------------------------------------------------------------
+    |
+    | This part holds all supported languages. These languages can be found
+    | inside the "resources/lang/" directory.
+    |
+    */
+
+    'supported_locales' => [
+        'en', 'de',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale
+    |--------------------------------------------------------------------------
+    |
+    | The (fallback) locale determines the locale to use. You may change
+    | the value to correspond to any of the "supported_locales" and
+    | language folders that are provided through the application.
+    |
+    */
+
+    'locale' => 'de',
+];
