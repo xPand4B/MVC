c9732705293f5effd92d3fa777ac884e5830526f
xPand4B <xpand.4beatz@gmail.com>
2019-03-19 21:42:30 +0100

    Added app.name

--- config/app.php
+++ config/app.php
@@ -9,6 +9,19 @@
 return [
     /*
     |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of the application. This value is used to place
+    | the application's name in a notification or any other location as
+    | required by the application.
+    |
+    */
+
+    'name' => env('APP_NAME'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Supported application locales
     |--------------------------------------------------------------------------
     |
