fc9625d68ea28e2a0df5c47a4c4507ee60109041
xPand4B <xpand.4beatz@gmail.com>
2019-03-23 12:54:54 +0100

    Added meta information + Included helper

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,10 +1,13 @@
 <meta charset="UTF-8">
+
+<meta name="description" content="Eric Heinzl Portfolio">
+<meta name="keywords" content="Eric,Heinzl,Programming,Web,Development,HTML,CSS,PHP,Portfolio">
+
 <meta name="author" content="Eric Heinzl">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<link rel="stylesheet" href="../css/main.css">
-
+{{ style('main.css') }}
 
 <title>{{ config('app.name') }} @yield('title')</title>
