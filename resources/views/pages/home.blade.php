b57f02acc1138fce0bc1d318e0cecec51d6bd3ff
xPand4B <xpand.4beatz@gmail.com>
2019-03-24 19:55:15 +0100

    Tested helper

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     Homepage
+
+    <br>
+    
+    {{-- Output: /mnt/Private/Programm Files/Google Drive/Private/WebDev/01-BKR/2018-19/02-Portfolio/public/img/collections/01-First-Steps/first-time-html.png --}}
+    {{-- {{ collection_image('01-First-Steps', 'first-time-html.png') }} --}}
+
+    {{ collection_image('01-First-Steps/first-time-html.png') }}
+   
+
 @endsection
